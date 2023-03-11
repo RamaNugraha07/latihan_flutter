b378a537133d69cb03879b5c654838c6941ce94a
Wardah Zahra <5wardahzahra@gmail.com>
2023-03-11 11:59:33 +0700

    Update connection.php

--- sqlConnection/connection.php
+++ sqlConnection/connection.php
@@ -1,3 +1,7 @@
 <?php 
+$conn = new mysqli("103.184.19.8:3306","internsql","garuda@2023!","user");
+$query = mysqli_query($conn,"select * from user");
+$data = mysqli_fetch_all($query,MYSQLI_ASSOC);
+echo json_decode($data);
 
 ?>
