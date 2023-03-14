640a3d4678424207c34b1652972bbbf678bb65f7
RamaNugraha07 <ramanugraha712@gmail.com>
2023-03-14 10:41:42 +0700

    Buat 4 card widget untuk dashboard

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 
 class Dashboard extends StatelessWidget {
   const Dashboard({super.key});
+
+
   
   @override
   Widget build(BuildContext context) {
@@ -16,9 +18,10 @@ class Dashboard extends StatelessWidget {
         children: <Widget>[
           Card(
             color: Color(0xFFDEE3F2),
-            elevation: 15,
             shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30)),
+              borderRadius: BorderRadius.circular(40)),
+              elevation: 10,
+              margin: const EdgeInsets.all(20),
               child: InkWell(
                 onTap: (){},
                 splashColor: Colors.lightBlueAccent,
@@ -27,7 +30,7 @@ class Dashboard extends StatelessWidget {
                     mainAxisSize: MainAxisSize.min,
                     children: <Widget>[
                       ClipRRect(
-                        borderRadius: BorderRadius.circular(50.0),
+                        borderRadius: BorderRadius.circular(5.0),
                         child: Image.asset('lib/icons/input.png')),
                       Text('Input', style: TextStyle(fontSize: 15.0),),
                       
@@ -40,91 +43,81 @@ class Dashboard extends StatelessWidget {
 
           Card(
             color: Color(0xFFDEE3F2),
-            margin: const EdgeInsets.all(4),
-            child: InkWell(
-              onTap: (){},
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(40)),
+              elevation: 10,
+              margin: const EdgeInsets.all(20),
+              child: InkWell(
+                onTap: (){},
+                splashColor: Colors.lightBlueAccent,
                 child: Center(
                   child: Column(
                     mainAxisSize: MainAxisSize.min,
-                    children: const <Widget>[
-                      Icon(Icons.home),
+                    children: <Widget>[
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(5.0),
+                        child: Image.asset('lib/icons/upload.png')),
+                      Text('Update', style: TextStyle(fontSize: 15.0),),
+                      
                     ],
                   ),
                 ),
-            ),
+              ),
 
-          )
+          ),
+          
+          Card(
+            color: Color(0xFFDEE3F2),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(40)),
+              elevation: 10,
+              margin: const EdgeInsets.all(20),
+              child: InkWell(
+                onTap: (){},
+                splashColor: Colors.lightBlueAccent,
+                child: Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(5.0),
+                        child: Image.asset('lib/icons/persebaran.png')),
+                      Text('Persebaran', style: TextStyle(fontSize: 15.0),),
+                      
+                    ],
+                  ),
+                ),
+              ),
+
+          ),
+
+          Card(
+            color: Color(0xFFDEE3F2),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(40)),
+              elevation: 10,
+              margin: const EdgeInsets.all(20),
+              child: InkWell(
+                onTap: (){},
+                splashColor: Colors.lightBlueAccent,
+                child: Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(5.0),
+                        child: Image.asset('lib/icons/lokasi.png')),
+                      Text('Lokasi', style: TextStyle(fontSize: 15.0),),
+                      
+                    ],
+                  ),
+                ),
+              ),
+
+          ),
+
         ],
       ),
       ); 
-  }
-}
-
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({super.key});
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Home',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: Business',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: School',
-      style: optionStyle,
-    ),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-  
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-        title: const Text('Profile'),
-      ),
-      body: Center(
-        // child: ElevatedButton(
-        //   onPressed: () {
-        //     Navigator.pop(context);
-        //   },
-        //   child: const Text("Go Back!"),
-        // ),
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Business',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'School',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
-      ),
-    );
   }
 }
