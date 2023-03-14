25d4473984d72a9425c41c48704ad77030e19019
auhppy <91692012+auhppy@users.noreply.github.com>
2023-03-15 06:21:52 +0700

    Update profile.dart

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,86 +1,198 @@
 import 'package:Sales/dashboard.dart';
 import 'package:flutter/material.dart';
-import 'package:Sales/cons.dart';
 import 'package:flutter/widgets.dart';
-
-
+import 'package:Sales/dashboard.dart';
 
 class Profile extends StatelessWidget{
   const Profile ({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-    );
-  }
-
-}
-
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({super.key});
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Home',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: Business',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: School',
-      style: optionStyle,
-    ),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-  
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-        title: const Text('Profile'),
-      ),
-      body: Center(
-        // child: ElevatedButton(
-        //   onPressed: () {
-        //     Navigator.pop(context);
-        //   },
-        //   child: const Text("Go Back!"),
-        // ),
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Business',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'School',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
+      body: Container( 
+        color: Color.fromRGBO(242, 241, 246, 1),
+        child: Padding(
+          padding: EdgeInsets.only(top: 50, bottom: 50), 
+          child: Column( 
+            children:[
+              Container(
+                padding: EdgeInsets.only(right: 25, left: 25),
+                height: 64,
+                child: Row(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [ CircleAvatar(
+                      radius: 32,
+                      backgroundImage: AssetImage('images/default-pp.png'),
+                    )]),
+                    SizedBox(width: 16),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text('Sales Dua', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),
+                        Text('SL0002', style: TextStyle(fontSize: 16)),
+                      ],
+                    ),
+                    SizedBox(width: 120),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Align(
+                          alignment: Alignment.centerRight,
+                            child: Container(
+                              width: 75,
+                              height: 30,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(30),
+                                color: Color.fromRGBO(254, 164, 30, 1),
+                              ),
+                              child: Center( 
+                                child: Text('Level 2', style: TextStyle(color: Colors.white),)
+                            )
+                          )
+                        )
+                      ]
+                    )
+                  ]
+                )
+              ),
+              SizedBox(height: 40),
+              Container(
+                padding: EdgeInsets.only(right: 0, left: 0),
+                width: MediaQuery.of(context).size.width,
+                height: 40,
+                color: Color.fromRGBO(222, 227, 242, 1),
+                alignment: Alignment.centerLeft,
+                child: Row(
+                  children: [
+                    SizedBox(width: 25),
+                    Text('Menu', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold,))
+                  ]
+                ),
+              ),
+              SizedBox(height: 20),
+              InkWell(
+                onTap: (){},
+                child: Container(
+                  padding: EdgeInsets.only(right: 25, left: 25),
+                  width: MediaQuery.of(context).size.width,
+                  height: 30,
+                  alignment: Alignment.centerLeft,
+                  child: Row(
+                    children: [
+                      Icon(Icons.maps_home_work),
+                      SizedBox(width: 10),
+                      Text('Lokasi Sales', style: TextStyle(fontSize: 15))
+                    ]
+                  ),
+                ),
+              ),
+              SizedBox(height:20),
+              InkWell(
+                onTap: (){},
+                child: Container(
+                  padding: EdgeInsets.only(right: 25, left: 25),
+                  width: MediaQuery.of(context).size.width,
+                  height: 30,
+                  alignment: Alignment.centerLeft,
+                  child: Row(
+                    children: [
+                      Icon(Icons.map),
+                      SizedBox(width: 10),
+                      Text('Persebaran Brosur', style: TextStyle(fontSize: 15))
+                    ]
+                  )
+                )
+              ),
+              SizedBox(height: 40),
+              Container(
+                child: Container(
+                  padding: EdgeInsets.only(right: 0, left: 0),
+                  width: MediaQuery.of(context).size.width,
+                  height: 40,
+                  color: Color.fromRGBO(222, 227, 242, 1),
+                  alignment: Alignment.centerLeft,
+                  child: Row(
+                    children: [
+                      SizedBox(width: 25),
+                      Text('Lainnya', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold,))
+                    ]
+                  )
+                )
+              ),
+              SizedBox(height: 20),
+              InkWell(
+                onTap: (){},
+                child: Container(
+                  padding: EdgeInsets.only(right: 25, left: 25),
+                  width: MediaQuery.of(context).size.width,
+                  height: 30,
+                  alignment: Alignment.centerLeft,
+                  child: Row(
+                    children: [
+                      Icon(Icons.info),
+                      SizedBox(width: 10),
+                      Text('Tentang', style: TextStyle(fontSize: 15))
+                    ]
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+                InkWell(
+                onTap: (){},
+                child: Container(
+                  padding: EdgeInsets.only(right: 25, left: 25),
+                  width: MediaQuery.of(context).size.width,
+                  height: 30,
+                  alignment: Alignment.centerLeft,
+                  child: Row(
+                    children: [
+                      Icon(Icons.book),
+                      SizedBox(width: 10),
+                      Text('Buku Panduan', style: TextStyle(fontSize: 15))
+                    ]
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              InkWell(
+                onTap: (){},
+                child: Container(
+                  padding: EdgeInsets.only(right: 25, left: 25),
+                  width: MediaQuery.of(context).size.width,
+                  height: 30,
+                  alignment: Alignment.centerLeft,
+                  child: Row(
+                    children: [
+                      Icon(Icons.key),
+                      SizedBox(width: 10),
+                      Text('Ganti Password', style: TextStyle(fontSize: 15))
+                    ]
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              InkWell(
+                onTap: (){},
+                child: Container(
+                  padding: EdgeInsets.only(right: 25, left: 25),
+                  width: MediaQuery.of(context).size.width,
+                  height: 30,
+                  alignment: Alignment.centerLeft,
+                  child: Row(
+                    children: [
+                      Icon(Icons.logout_rounded),
+                      SizedBox(width: 10),
+                      Text('Keluar', style: TextStyle(fontSize: 15))
+                    ]
+                  ),
+                ),
+              ),
+            ] 
+          )
+        ),
       ),
     );
   }
