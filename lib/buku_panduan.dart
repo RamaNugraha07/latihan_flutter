4cc87fa5d8b32ad0d7bbdf66ca7d971366115567
auhppy <91692012+auhppy@users.noreply.github.com>
2023-03-16 09:32:21 +0700

    tambah halaman bukpan + navigator

---
+++ lib/buku_panduan.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class BukuPanduan extends StatelessWidget {
+  const BukuPanduan ({super.key});  
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: const Text('Tracker'),
+      // ),
+      body: Center(
+      child: Text('Cooming Soon'),
+      )
+    );
+  }
+}
