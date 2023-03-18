5edf400eac9c0efcea7a3296a05deeeb1176dfa8
RamaNugraha07 <ramanugraha712@gmail.com>
2023-03-18 13:29:20 +0700

    Sales, Update dibuar cooming soon, buat new folder

---
+++ lib/dashboard/update.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Update extends StatelessWidget{
+    const Update ({super.key});
+
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+        title: const Text('Update Customer'),
+      ),
+        body: Center(
+          child: Text('Cooming Soon'),
+        ),
+      );
+    }
+}
